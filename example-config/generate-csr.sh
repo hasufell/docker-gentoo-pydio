b8a7a88c4f0468017a45c249f0047cb544e0eb06
hasufell <hasufell@hasufell.de>
2015-08-30 03:00:01 +0200

    Fix generate-csr.sh script

--- example-config/generate-csr.sh
+++ example-config/generate-csr.sh
@@ -1,3 +1,3 @@
 # see: https://www.digicert.com/csr-creation-apache.htm
 
-openssl req -new -newkey rsa:2048 -nodes -keyout ssl/certs/server.key -out ssl/certs/server.csr
+openssl req -new -newkey rsa:2048 -nodes -keyout ssl/server/server.key -out ssl/server/server.csr
