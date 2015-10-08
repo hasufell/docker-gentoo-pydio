35cbe44f9ff0b15f20638c53912d1dcfb3db4569
hasufell <hasufell@hasufell.de>
2015-10-08 16:59:43 +0200

    Improve start script, this should be more safe

--- core/start.sh
+++ core/start.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
-if [[ ! -e /var/lib/pydio/public/index.html ]] ; then
-	mkdir -p /var/lib/pydio
+dir_is_empty() {
+	# usage:
+	#  dir_is_empty <some-dir>
+	#
+	# returns 2 if the dir does not even exist
+	# returns 1 if the dir is not empty
+	# returns 0 (success) if the dir exists and is empty
+
+	local dir=$1
+	local files
+
+	if [[ ! -e ${dir} ]] ; then
+		return 2
+	fi
+
+	shopt -s nullglob dotglob     # To include hidden files
+
+	files=( "${dir}"/* )
+
+	if [[ ${#files[@]} -eq 0 ]]; then
+		return 0
+	else
+		return 1
+	fi
+
+	shopt -u nullglob dotglob
+}
+
+# if folders are empty, they are probably mounted in from the host,
+# so we sync them
+
+if dir_is_empty /var/lib/pydio ; then
+	echo ""
+	echo "Syncing /var/lib/pydio"
+	echo ""
+
 	chown -R :www /var/lib/pydio
 	chmod -R g+w /var/lib/pydio
 	rsync -a /var/lib/pydio-orig/* /var/lib/pydio/
 fi
 
-if [[ ! -e /var/cache/pydio/index.html ]] ; then
-	mkdir -p /var/cache/pydio
+if dir_is_empty /var/cache/pydio ; then
+	echo ""
+	echo "Syncing /var/cache/pydio"
+	echo ""
+
 	chown -R :www /var/cache/pydio/
 	chmod -R g+w /var/cache/pydio/
 	rsync -a /var/cache/pydio-orig/* /var/cache/pydio/
